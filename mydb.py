718a842b562039a210989dad1d2ad30ab77bb68e
Monika Bhadauriya <bhadauriyamonika25@gmail.com>
2025-10-30 12:18:40 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,22 @@
+#Install Mysql on your computer
+#pip install mysql
+#pip install mysql-computer
+#pip install mysql-connector-python.
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+	host = 'localhost',
+	user = 'root',
+	passwd = 'monikabh25'
+	
+	)
+
+#prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# Create a database
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("All Done!")
+
